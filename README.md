f38665ad4f95edfd6baf2b4d68a3f3fe46515303
arvaidya <arvaidya@gmail.com>
2016-01-27 15:48:49 +0530

    added test cases

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # barCharts
-bar charts example
+bar charts example using Reactive js
+
+Installation: 
+
+1. open npm console
+2. clone the git url 
+3. navigate to project folder
+4. run npm install
+
+Running project:
+
+1. run command 'grunt serve' in npm console
+2. in browser open 'http://localhost:9999/views/index.html'
+
+Testing project:
+
+1. in npm console run command 'npm test'
+
+
--- test/test.js
+++ test/test.js
@@ -1,9 +1,19 @@
 
 var expect  = require('chai').expect;
+//var barcharts  = require('../scripts/barcharts');
 
 describe('A basic test', function () {
 
 	it('should pass when everything is ok', function  () {
 		expect(true).to.be.true;
 	});
+
+
+	it('testing js calculation function', function  () {
+		
+		/*var result = barcharts.changeBar(10);
+		console.log("res:   " +result);*/
+
+	});
+
 });
